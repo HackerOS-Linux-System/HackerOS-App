f40c05c217cc0e91684551f6ea166e24830db694
michal92299 <chudybyczek2137@gmail.com>
2026-01-24 23:48:19 +0100

    Create constants.ts

---
+++ source-code/constants.ts
@@ -0,0 +1,47 @@
+// Note: Converted github blob links to raw links to ensure they load correctly in the app
+export const RELEASE_INFO_URL = 'https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-App/main/release-info.hacker';
+
+export const WALLPAPERS = [
+  {
+    id: 1,
+    name: "Default HackerOS",
+    url: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/default-wallpaper.png",
+    thumbnail: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/default-wallpaper.png"
+  },
+  {
+    id: 2,
+    name: "Cyber Grid",
+    url: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper.png",
+    thumbnail: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper.png"
+  },
+  {
+    id: 3,
+    name: "Neon Nights",
+    url: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper1.png",
+    thumbnail: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper1.png"
+  },
+  {
+    id: 4,
+    name: "Abstract Flow",
+    url: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper2.png",
+    thumbnail: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper2.png"
+  },
+  {
+    id: 5,
+    name: "Deep Space",
+    url: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper3.png",
+    thumbnail: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper3.png"
+  },
+  {
+    id: 6,
+    name: "Code Rain",
+    url: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper4.png",
+    thumbnail: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper4.png"
+  },
+  {
+    id: 7,
+    name: "Circuitry",
+    url: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper5.png",
+    thumbnail: "https://raw.githubusercontent.com/HackerOS-Linux-System/HackerOS-Website/main/phone-wallpapers/wallpaper5.png"
+  }
+];
