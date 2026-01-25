7baad46a49e9e7d2f171a2f92132abebd205df0a
michal92299 <chudybyczek2137@gmail.com>
2026-01-25 14:28:19 +0100

    Update capacitor.config.ts

--- source-code/capacitor.config.ts
+++ source-code/capacitor.config.ts
@@ -1,12 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'com.hackeros.app',
-    appName: 'HackerOS App',
-    webDir: 'dist',
-    server: {
-        androidScheme: 'https'
-    }
+  appId: 'com.hackeros.app',
+  appName: 'HackerOS App',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
