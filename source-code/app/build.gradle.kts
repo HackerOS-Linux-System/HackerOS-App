0ea5f465f87b58a067bb671324e486523c953e37
michal92299 <chudybyczek2137@gmail.com>
2026-09-24 21:28:22 +0200

    Update build.gradle.kts

--- source-code/app/build.gradle.kts
+++ source-code/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hackeros.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.6"
+        versionCode = 10
+        versionName = "0.8"
         vectorDrawables { useSupportLibrary = true }
     }
 
